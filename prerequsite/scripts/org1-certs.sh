4b15adb259fbb1ba32bc0173cca0ad0caf66791e
swamshraddha <80090983+swamshraddha@users.noreply.github.com>
2023-08-03 18:03:41 +0530

    Update org1-certs.sh

--- prerequsite/scripts/org1-certs.sh
+++ prerequsite/scripts/org1-certs.sh
@@ -76,3 +76,25 @@ fabric-ca-client enroll -u https://org1admin:org1adminpw@ca-org1:7054 --caname c
 cp "/organizations/peerOrganizations/org1.example.com/msp/config.yaml" "/organizations/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/config.yaml"
 
 { set +x; } 2>/dev/null
+
+
+
+fabric-ca-client register --caname ca-org1 --id.name peer1 --id.secret peer1pw --id.type peer --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
+fabric-ca-client enroll -u https://peer1:peer1pw@ca-org1:7054 --caname ca-org1 -M "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp" --csr.hosts peer1.org1.example.com --csr.hosts  peer1-org1 --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
+cp "/organizations/peerOrganizations/org1.example.com/msp/config.yaml" "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/msp/config.yaml"
+fabric-ca-client enroll -u https://peer1:peer1pw@ca-org1:7054 --caname ca-org1 -M "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls" --enrollment.profile tls --csr.hosts peer1.org1.example.com --csr.hosts  peer1-org1 --csr.hosts ca-org1 --csr.hosts localhost --tls.certfiles "/organizations/fabric-ca/org1/tls-cert.pem"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/tlscacerts/"* "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/signcerts/"* "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.crt"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/keystore/"* "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/server.key"
+
+mkdir -p "/organizations/peerOrganizations/org1.example.com/msp/tlscacerts"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/tlscacerts/"* "/organizations/peerOrganizations/org1.example.com/msp/tlscacerts/ca.crt"
+
+mkdir -p "/organizations/peerOrganizations/org1.example.com/tlsca"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/tlscacerts/"* "/organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem"
+
+mkdir -p "/organizations/peerOrganizations/org1.example.com/ca"
+cp "/organizations/peerOrganizations/org1.example.com/peers/peer1 .org1.example.com/msp/cacerts/"* "/organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem"
+
+# mkdir -p organizations/peerOrganizations/org1.example.com/peers/peer1.org1.example.com
+{ set +x; } 2>/dev/null
