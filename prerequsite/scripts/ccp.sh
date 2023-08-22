5db5d73d846ea12301f5da6e207ad0c4a3e5c44f
swamshraddha <80090983+swamshraddha@users.noreply.github.com>
2023-08-22 12:29:29 +0530

    Update ccp.sh

--- prerequsite/scripts/ccp.sh
+++ prerequsite/scripts/ccp.sh
@@ -9,6 +9,7 @@ function json_ccp {
     local CP=$(one_line_pem $5)
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
+        -e "s/\${P1PORT}/$6/" \        
         -e "s/\${CAPORT}/$3/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
@@ -20,6 +21,7 @@ function yaml_ccp {
     local CP=$(one_line_pem $5)
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
+        -e "s/\${P1PORT}/$6/" \       
         -e "s/\${CAPORT}/$3/" \
         -e "s#\${PEERPEM}#$PP#" \
         -e "s#\${CAPEM}#$CP#" \
@@ -28,30 +30,32 @@ function yaml_ccp {
 
 ORG=1
 P0PORT=7051
+P1PORT=8051
 CAPORT=7054
 PEERPEM=organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-profile/connection-org1.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-profile/connection-org1.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $P1PORT)" > connection-profile/connection-org1.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $P1PORT)" > connection-profile/connection-org1.yaml
+
 
 ORG=2
 P0PORT=9051
+P1PORT=8051
 CAPORT=8054
 PEERPEM=organizations/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org2.example.com/ca/ca.org2.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-profile/connection-org2.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-profile/connection-org2.yaml
-
-
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $P1PORT)" > connection-profile/connection-org2.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $P1PORT)" > connection-profile/connection-org2.yaml
 
 
 ORG=3
 P0PORT=11051
+P1PORT=8051
 CAPORT=9054
 PEERPEM=organizations/peerOrganizations/org3.example.com/tlsca/tlsca.org3.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org3.example.com/ca/ca.org3.example.com-cert.pem
 
-echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-profile/connection-org3.json
-echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > connection-profile/connection-org3.yaml
+echo "$(json_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $P1PORT)" > connection-profile/connection-org3.json
+echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM $P1PORT)" > connection-profile/connection-org3.yaml
